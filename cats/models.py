c8d254a1207a919c16c64da5bee94965314d0a47
testingworld <tania.pereminski@gmail.com>
2026-05-15 08:57:14 -0700

    Defined the model/structure of my Cat data

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Cat(models.Model):
+    name=models.CharField(max_length=100)
+    kind=models.CharField(max_length=100) # e.g., Persian, Siamese
+    age = models.ImageField()
+    weight = models.FloatField()
+    vaccinated= models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
