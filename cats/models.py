e9fca160d2d3b919f2b566738689bac98595af4c
testingworld <tania.pereminski@gmail.com>
2026-05-15 09:37:24 -0700

    Create the databasr schema by migration

--- cats/models.py
+++ cats/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Cat(models.Model):
     name=models.CharField(max_length=100)
     kind=models.CharField(max_length=100) # e.g., Persian, Siamese
-    age = models.ImageField()
+    age = models.IntegerField()
     weight = models.FloatField()
     vaccinated= models.BooleanField(default=False)
     
--- cats/views.py
+++ cats/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Cat
 from .serializers import CatSerializer
 
-class CatViewSet(viewsets.ModeViewSet):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_with = CatSerializer
     serializer_class = CatSerializer
